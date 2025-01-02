e1434ad75e5581afff5dae0ca0d7f7ed9adfd215
Atharv Joshi <atharvj07@gmail.com>
2025-01-02 15:45:08 +0530

    webhooks added

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/mogodb');
-
+const userRoute = require('./routes/userRoutes')
 //app config
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -15,5 +15,6 @@ app.use(express.json());
 
 //Api routes
 app.get('/',(req,res)=> res.send("API Working"));
+app.use('/api/user',userRoute)
 
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
